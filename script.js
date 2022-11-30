d3dd3e180d3fded8f8392640094a2ff1ba3111b9
Islom <islom@gmail.com>
2022-11-30 14:02:14 +0300

    .

---
+++ script.js
@@ -0,0 +1,16 @@
+// var a = +prompt("Son kirit")
+// while (isNaN(a)) {
+//     a= +prompt("Son kiritmasen foydasi yo")
+// }
+// alert ("Vanihoyat son kiritding "+a)
+
+
+do {
+    var a = +prompt ("Son kirit")
+} while (isNaN(a));
+
+if (a==0 || a===0) {
+    alert ("faqat nolmas")
+}
+
+
